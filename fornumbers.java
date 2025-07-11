3bc7b70674ec1f5ac6c5f6e71c18fbee1e509241
Gaggu_01 <gargeydhundale01@gmail.com>
2025-07-11 19:22:03 +0530

    Add files via upload

---
+++ fornumbers.java
@@ -0,0 +1,14 @@
+public class fornumbers {
+    public static void main(String[] args) {
+        int n = 5;
+        number(n);
+    }
+    static void number(int n ){
+        for(int i = 1 ; i <= n ; i++){
+            for (int j = 1 ; j <= i ; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
